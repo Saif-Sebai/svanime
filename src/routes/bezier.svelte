6e86053f252b520a6eb160f34e87b146ba5d4061
Saif Sebai <saif.sebai@ensi-uma.tn>
2025-01-06 14:25:08 +0100

    cool animation

--- src/routes/bezier.svelte
+++ src/routes/bezier.svelte
@@ -8,15 +8,37 @@
 	let points: Point[] = [];
 	// 1920 x 1080
 	points.push({ x: 200, y: 200 });
-	points.push({ x: 900, y: 900 });
-	points.push({ x: 1600, y: 700 });
+	points.push({ x: 900, y: 1600 });
+	points.push({ x: 1900, y: 700 });
 	points.push({ x: 1020, y: 1080 - 900 });
+	points.push(points[0]);
+	points.push(points[2]);
+	// points.push(points[1]);
 
-	function bezier(t: number, p1: Point, p2: Point): Point {
+	/**
+	 * Linear interpolation between two points.
+	 */
+	function lerp(t: number, p0: Point, p1: Point): Point {
 		return {
-			x: Number(((1 - t) * p1.x + t * p2.x).toFixed(5)),
-			y: Number(((1 - t) * p1.y + t * p2.y).toFixed(5)) //ok maybe no need for toFixed...
+			x: (1 - t) * p0.x + t * p1.x,
+			y: (1 - t) * p0.y + t * p1.y
 		};
+	}
+
+	/**
+	 * Generalized Bézier function for n control points.
+	 */
+	function bezier(t: number, controlPoints: Point[]): Point {
+		if (controlPoints.length === 1) {
+			return controlPoints[0];
+		}
+
+		const newPoints: Point[] = [];
+		for (let i = 0; i < controlPoints.length - 1; i++) {
+			newPoints.push(lerp(t, controlPoints[i], controlPoints[i + 1]));
+		}
+
+		return bezier(t, newPoints); // Recursive call
 	}
 
 	function drawLine(p1: Point, p2: Point): { length: number; angle: number } {
@@ -28,42 +50,38 @@
 		};
 	}
 
-	function travel(t: number, points: Point[], pos: Point): [number, Point] {
-		t += 0.01;
-		q0 = bezier(t, points[0], points[1]);
-		q1 = bezier(t, points[1], points[2]);
-		pos = bezier(t, q0, q1);
-		// console.log(t);
-		if (t > 1) {
-			console.log('GG');
+	// function travel(t: number, points: Point[], pos: Point): [number, Point] {
+	// 	t += 0.01;
+	// 	q0 = bezier(t, points[0], points[1]);
+	// 	q1 = bezier(t, points[1], points[2]);
+	// 	pos = bezier(t, q0, q1);
+	// 	// console.log(t);
+	// 	if (t > 1) {
+	// 		console.log('GG');
 
-			t = 0;
-		}
-		return [t, pos];
-	}
+	// 		t = 0;
+	// 	}
+	// 	return [t, pos];
+	// }
 
 	// Start the animation
 	let t = 0;
-	let q0 = bezier(t, points[0], points[1]);
-	let q1 = bezier(t, points[1], points[2]);
-	let b = bezier(t, q0, q1);
+	// let q0 = bezier(t, points[0], points[1]);
+	// let q1 = bezier(t, points[1], points[2]);
+	// let b = bezier(t, q0, q1);
 	let k = 0;
 	let alpha = 0;
+	let b: Point = points[0];
 
 	function animate() {
 		// Use requestAnimationFrame for smooth animations
 		function frame() {
 			// t = Math.cos(alpha);
-			alpha += 0.01 * Math.PI;
-			console.log(t);
-
-			if (Math.sin(alpha) > 0) {
-				let back = points.slice(2);
-				back.push(points[0]);
-				[t, b] = travel(t, back, b);
-			} else {
-				[t, b] = travel(t, points, b);
+			t += 0.01;
+			if (t > 1) {
+				t = 0;
 			}
+			b = bezier(t, points);
 
 			window.requestAnimationFrame(frame);
 			// [t, b] = travel(t, points, b);
@@ -89,9 +107,9 @@
 	alt=""
 	src="https://www.gifservice.fr/img/gif-vignette-large/a6d3d0463855c0a5c5e6ecfc2366d5f0/273082-d-v-d-video-video-iconos-multimedia.gif"
 />
-<h1 style="max-height:10%;bottom:{points[0].y}px;left:{points[0].x}px;position:absolute;">O</h1>
-<h1 style="max-height:10%;bottom:{points[1].y}px;left:{points[1].x}px;position:absolute;">O</h1>
-<h1 style="max-height:10%;bottom:{points[2].y}px;left:{points[2].x}px;position:absolute;">O</h1>
-<h1 style="max-height:10%;bottom:{points[3].y}px;left:{points[3].x}px;position:absolute;">O</h1>
-<h1 style="max-height:10%;bottom:{q0.y}px;left:{q0.x}px;position:absolute;">O</h1>
-<h1 style="max-height:10%;bottom:{q1.y}px;left:{q1.x}px;position:absolute;">O</h1>
+<h1 style="max-height:10%;bottom:{points[0].y}px;left:{points[0].x}px;position:absolute;">.</h1>
+<h1 style="max-height:10%;bottom:{points[1].y}px;left:{points[1].x}px;position:absolute;">.</h1>
+<h1 style="max-height:10%;bottom:{points[2].y}px;left:{points[2].x}px;position:absolute;">.</h1>
+<h1 style="max-height:10%;bottom:{points[3].y}px;left:{points[3].x}px;position:absolute;">.</h1>
+<!-- <h1 style="max-height:10%;bottom:{q0.y}px;left:{q0.x}px;position:absolute;">O</h1> -->
+<!-- <h1 style="max-height:10%;bottom:{q1.y}px;left:{q1.x}px;position:absolute;">O</h1> -->
