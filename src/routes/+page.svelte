3041a2d5ec845aa63b73f5fa06ee9a15ac963a09
Saif Sebai <saif.sebai@ensi-uma.tn>
2025-01-06 12:15:54 +0100

    Added deployment to GithubPages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import Bezier from './bezier.svelte';
 	import Dvd from './dvd.svelte';
+
+	// This can be false if you're using a fallback (i.e. SPA mode)
+	export const prerender = true;
 </script>
 
 <!-- <h1>Welcome to SvelteKit</h1>
